8e4df71027c06c0c9d00eab054c61adb8f94d4df
Olha Kostenko <osbkin@gmail.com>
2021-12-10 17:22:04 +0200

    init commit

---
+++ test_1/test_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_1
+//
+//  Created by Olha Kostenko on 2021-12-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
